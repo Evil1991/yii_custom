5829b16fdaf9cd893bfbef69465d19f2085df358
Казанцев Александр <konsul117@bk.ru>
2016-10-25 01:20:51 +1000

    Функция-хелпер для транслитерации

--- yiiCustom/helpers/StringHelper.php
+++ yiiCustom/helpers/StringHelper.php
@@ -90,4 +90,68 @@ class StringHelper extends \yii\helpers\StringHelper {
 		return strcmp(mb_strtoupper($str1, $encoding), mb_strtoupper($str2, $encoding)) === 0;
 	}
 
+	/**
+	 * Генерация транслитированной строки.
+	 *
+	 * @param string
+	 * @return string
+	 */
+	protected function transliterate($title) {
+		$title = preg_replace('/\[([^\]]+)\]/u', '', $title);
+		$title = preg_replace('/\(([^\)]+)\)/u', '', $title);
+
+		$translit = [
+			'/'  => '-',
+			'\\' => '-',
+			' '  => '-',
+			'а'  => 'a',
+			'б'  => 'b',
+			'в'  => 'v',
+			'г'  => 'g',
+			'д'  => 'd',
+			'е'  => 'e',
+			'ё'  => 'yo',
+			'ж'  => 'zh',
+			'з'  => 'z',
+			'и'  => 'i',
+			'й'  => 'j',
+			'к'  => 'k',
+			'л'  => 'l',
+			'м'  => 'm',
+			'н'  => 'n',
+			'о'  => 'o',
+			'п'  => 'p',
+			'р'  => 'r',
+			'с'  => 's',
+			'т'  => 't',
+			'у'  => 'u',
+			'ф'  => 'f',
+			'х'  => 'x',
+			'ц'  => 'c',
+			'ч'  => 'ch',
+			'ш'  => 'sh',
+			'щ'  => 'shh',
+			'ы'  => 'y',
+			'э'  => 'e',
+			'ю'  => 'yu',
+			'я'  => 'ya',
+			'ь'  => '',
+			'ъ'  => '',
+			'-'  => '-',
+		];
+
+		$title = mb_strtolower($title, 'UTF-8');
+
+		$title = str_replace(array_keys($translit), array_values($translit), $title);
+
+		$title = preg_replace('/[^\-_A-z0-9]/u', '', $title);
+		$title = str_replace('-[]', '', $title);
+		$title = str_replace('[', '', $title);
+		$title = str_replace(']', '', $title);
+		$title = trim($title, '-');
+		$title = preg_replace('/-+/', '-', $title);
+
+		return $title;
+	}
+
 }
