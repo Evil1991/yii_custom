04905bdc0f58d57854d32029f234f9b5de67a798
Казанцев Александр <konsul117@bk.ru>
2016-11-28 04:04:07 +1000

    добавлен логгер в файл

---
+++ logger/FileLogger.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace yiiCustom\logger;
+
+use yiiCustom\base\DateTimeZone;
+use yiiCustom\helpers\StringHelper;
+use Yii;
+use yii\base\Component;
+use yii\helpers\Console;
+
+class FileLogger extends Component implements LoggerStream {
+	/** Разделитель между полями вывода */
+	const FIELD_DIVIDER = "\t";
+
+	/** @var bool Вывод даты-времени */
+	public $dateTimeOut = true;
+	const ATTR_DATE_TIME_OUT = 'dateTimeOut';
+
+	/** @var string Формат даты-времени вывода */
+	public $dateTimeFormat = 'd.m.Y H:i:s';
+	const ATTR_DATE_TIME_FORMAT = 'dateTimeFormat';
+
+	/** @var bool Вывод использования памяти */
+	public $memoryUsageOut = true;
+	const ATTR_MEMORY_USAGE_OUT = 'memoryUsageOut';
+
+	/** @var string Категория лога */
+	public $logCategory;
+	const ATTR_LOG_CATEGORY = 'logCategory';
+
+	/** @var DateTimeZone Таймзона для вывода времени */
+	protected $timezone;
+
+	/**
+	 * @inheritdoc
+	 */
+	public function init() {
+		parent::init();
+
+		$this->timezone = DateTimeZone::getTimezoneByUtcOffset(Yii::$app->params['localTimezoneOffset']);
+	}
+
+	/**
+	 * Добавление записи
+	 *
+	 * @inheritdoc
+	 */
+	public function log($logMessage, $type = self::TYPE_MESSAGE) {
+		$message = '';
+
+		if ($this->dateTimeOut) {
+			$message .= (new \DateTime('now', $this->timezone))->format($this->dateTimeFormat) . static::FIELD_DIVIDER;
+		}
+
+		if ($this->memoryUsageOut) {
+			$message  .= StringHelper::formatSize(memory_get_usage(true)) . ' б.' . static::FIELD_DIVIDER;
+		}
+
+		$message .= $logMessage;
+
+		Yii::info($message, $this->logCategory);
+	}
+}
